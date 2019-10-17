875ec2e33622825bbb2ee8b8f0ed21589fc12e08
gabrielgranata <ggran067@uottawa.ca>
2019-10-17 14:41:12 -0400

    adds user creation with firebase auth

--- EpicClinic/app/src/main/java/ca/uottawa/mali165/epicclinic/Activities/RegisterActivity.java
+++ EpicClinic/app/src/main/java/ca/uottawa/mali165/epicclinic/Activities/RegisterActivity.java
@@ -1,20 +1,74 @@
-package ca.uottawa.mali165.epicclinic;
+package ca.uottawa.mali165.epicclinic.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import ca.uottawa.mali165.epicclinic.MainActivity;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText firstNameEditText, lastNameEditText, emailEditText, phoneNumberEditText, passwordEditText;
+    Button registerButton;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        firstNameEditText = findViewById(R.id.firstNameEditText);
+        lastNameEditText = findViewById(R.id.lastNameEditText);
+        emailEditText = findViewById(R.id.phoneNumberEditText);
+        phoneNumberEditText = findViewById(R.id.phoneNumberEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+
+        registerButton = findViewById(R.id.registerBtn);
     }
 
     public void RegisterBtnClicked(View loginBtn){
+
+        String firstName = firstNameEditText.getText().toString();
+        String lastName = lastNameEditText.getText().toString();
+        String email = emailEditText.getText().toString();
+        String phoneNumber = phoneNumberEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        if (email.isEmpty()) {
+            emailEditText.setError("Please enter an email");
+            emailEditText.requestFocus();
+        } else if (password.isEmpty()) {
+            passwordEditText.setError("Please enter a password");
+            passwordEditText.requestFocus();
+        } else if (password.isEmpty() && email.isEmpty()) {
+            Toast.makeText(RegisterActivity.this, "Required Fields are Empty!", Toast.LENGTH_SHORT).show();
+        } else {
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // task is successful, do whatever you want with it
+                            } else {
+                                // sign in/up fails, display message to user
+                                Toast.makeText(RegisterActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+
+
         Intent openWelcomeWindow = new Intent(getApplicationContext(), WelcomeActivity.class);
         startActivity(openWelcomeWindow);
     }
