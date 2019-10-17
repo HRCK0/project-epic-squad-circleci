13843a9e6aa71730e1f0ed002e0c05832aa80efb
Mustafa Ali <alimustafa0987@gmail.com>
2019-10-17 11:48:34 -0400

    created main page

--- EpicClinic/app/src/main/java/ca/uottawa/mali165/epicclinic/MainActivity.java
+++ EpicClinic/app/src/main/java/ca/uottawa/mali165/epicclinic/MainActivity.java
@@ -2,7 +2,10 @@ package ca.uottawa.mali165.epicclinic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void loginBtnClicked(View loginBtn){
+        Intent openLoginWindow = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(openLoginWindow);
+    }
+
+    public void registerBtnClicked(View registerBtn){
+        Intent openRegisterBtn = new Intent(getApplicationContext(), RegisterActivity.class);
+        startActivity(openRegisterBtn);
+    }
+
+
 }
