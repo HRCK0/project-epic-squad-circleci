e0e5449db6bcacbbcd56e2848390510b947c8d15
gabrielgranata <ggran067@uottawa.ca>
2019-10-17 14:50:37 -0400

    adds sign in functionality

--- EpicClinic/app/src/main/java/ca/uottawa/mali165/epicclinic/Activities/LoginActivity.java
+++ EpicClinic/app/src/main/java/ca/uottawa/mali165/epicclinic/Activities/LoginActivity.java
@@ -1,21 +1,67 @@
 package ca.uottawa.mali165.epicclinic;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import ca.uottawa.mali165.epicclinic.Activities.RegisterActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText emailEditText, passwordEditText;
+    Button loginButton;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+
+        loginButton = findViewById(R.id.loginButton);
     }
     
     public void loginBtnClicked(View loginBtn){
-        Intent openWelcomeWindow = new Intent(getApplicationContext(), ca.uottawa.mali165.epicclinic.WelcomeActivity.class);
-        startActivity(openWelcomeWindow);
+
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        if (email.isEmpty()) {
+            emailEditText.setError("Please enter an email");
+            emailEditText.requestFocus();
+        } else if (password.isEmpty()) {
+            passwordEditText.setError("Please enter a password");
+            passwordEditText.requestFocus();
+        } else if (password.isEmpty() && email.isEmpty()) {
+            Toast.makeText(RegisterActivity.this, "Required Fields are Empty!", Toast.LENGTH_SHORT).show();
+        } else {
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // task is successful, do whatever you want with it
+                                Intent openWelcomeWindow = new Intent(getApplicationContext(), ca.uottawa.mali165.epicclinic.WelcomeActivity.class);
+                                startActivity(openWelcomeWindow);
+                            } else {
+                                // sign in/up fails, display message to user
+                                Toast.makeText(LoginActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
     }
 }
