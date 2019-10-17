2512a34404061e5a843fd9809409f3838aab6638
Georges Chamoun <gcham042@uottawa.ca>
2019-10-17 13:41:06 -0400

    Created Welcome Page

---
+++ EpicClinic/app/src/main/java/ca/uottawa/mali165/epicclinic/WelcomeActivity.java
@@ -0,0 +1,14 @@
+package ca.uottawa.mali165.epicclinic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
