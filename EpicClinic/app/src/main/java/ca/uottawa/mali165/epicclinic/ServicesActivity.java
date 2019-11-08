6d1db77278a381c654aca50878ea7541941a4173
Mustafa Ali <alimustafa0987@gmail.com>
2019-11-08 12:51:09 -0500

    updated styles of services page

--- EpicClinic/app/src/main/java/ca/uottawa/mali165/epicclinic/ServicesActivity.java
+++ EpicClinic/app/src/main/java/ca/uottawa/mali165/epicclinic/ServicesActivity.java
@@ -16,18 +16,11 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
-
-import org.w3c.dom.Document;
 
 import java.util.ArrayList;
 import java.util.HashMap;
