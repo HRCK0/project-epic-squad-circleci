e94eae60a495a7c316516228a8cd8fcfc0158bdc
Ruwani <ruwanide51@gmail.com>
2019-11-17 15:30:46 -0500

    added new complete user profile page

---
+++ EpicClinic/app/src/main/java/ca/uottawa/mali165/epicclinic/CompleteUserProfileActivity.java
@@ -0,0 +1,14 @@
+package ca.uottawa.mali165.epicclinic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompleteUserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complete_user_profile);
+    }
+}
