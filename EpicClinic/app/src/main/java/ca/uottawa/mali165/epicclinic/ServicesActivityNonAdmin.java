89ad9378316e89f8439b0bdf03bcf2a3e32f61ff
Georges Chamoun <gcham042@uottawa.ca>
2019-11-19 01:42:05 -0500

    Update ServicesActivityNonAdmin.java

--- EpicClinic/app/src/main/java/ca/uottawa/mali165/epicclinic/ServicesActivityNonAdmin.java
+++ EpicClinic/app/src/main/java/ca/uottawa/mali165/epicclinic/ServicesActivityNonAdmin.java
@@ -35,6 +35,9 @@ public class ServicesActivityNonAdmin extends AppCompatActivity {
     private static final String TAG = "ServicesNonAdmin";
 
     List<Service> serviceList = new LinkedList<>();
+    List<String> categoryList = new LinkedList<>();
+    List<String> serviceList2 = new LinkedList<>();
+
 
     ListView listView;
 
@@ -53,14 +56,39 @@ public class ServicesActivityNonAdmin extends AppCompatActivity {
         updateUI();
     }
 
-    public void showToast(String textToShow){
+    public void showToast(String textToShow) {
         Toast.makeText(ServicesActivityNonAdmin.this, textToShow, Toast.LENGTH_SHORT).show();
     }
 
     public void updateUI() {
         // implement code to update list with firebase services
 
+
         final Activity t = this;
+
+        db.collection("services")
+                .document("services").get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                        if (task.isSuccessful()) {
+                            Map categories = task.getResult().getData();
+
+                            for (Object category : categories.keySet()) {
+                                Map categoryData = (Map) categories.get(category);
+                                //Log.d(TAG, "ADDING TO LIST_CATEGORY: " + category);
+                                categoryList.add((String) category);
+
+                                for (Object service : categoryData.keySet()) {
+                                  //  Log.d(TAG, "ADDING TO LIST_SERVICE: " + service);
+                                    serviceList2.add((String) service);
+
+                                }
+                            }
+                        }
+                    }
+                });
 
         db.collection("users")
                 .document(getIntent().getStringExtra("CurrentUser_UID")).get()
@@ -73,26 +101,44 @@ public class ServicesActivityNonAdmin extends AppCompatActivity {
                             Map empData = task.getResult().getData();
                             if (empData.containsKey("Services")) {
                                 Map services = (Map) empData.get("Services");
+
                                 for (Object category : services.keySet()) {
+
                                     Map categoryData = (Map) services.get(category);
 
-                                    for (Object service : categoryData.keySet()) {
-                                        Map serviceData = (Map) categoryData.get(service);
+                                    if (categoryList.remove((String) category)) {
 
-                                        String serviceName = serviceData.get("name").toString();
-                                        String price = serviceData.get("price").toString();
-                                        list.add(new Service(serviceName, price, (String) category));
+                                        for (Object service : categoryData.keySet()) {
 
+                                            Map serviceData = (Map) categoryData.get(service);
+
+                                            if (serviceList2.remove((String) service)) {
+
+                                                String serviceName = serviceData.get("name").toString();
+                                                String price = serviceData.get("price").toString();
+                                                Log.d(TAG, "onComplete: ADDED TO MAIN LIST");
+                                                list.add(new Service(serviceName, price, (String) category));
+                                            } else {
+                                                Log.d(TAG, "onComplete: " + category);
+                                                //Log.d(TAG, "onComplete: " + service);
+                                                //deleteFromDB((String) category, (String) service);
+                                            }
+                                        }
+                                    } else {
+                                       // Log.d(TAG, "onComplete2: " + category);
+
+                                        //deleteFromDB((String) category, null);
                                     }
-
                                 }
                                 serviceList = list;
                             }
                             ServicesListViewAdapterNonAdmin servicesListViewAdapter = new ServicesListViewAdapterNonAdmin(t, serviceList);
                             listView.setAdapter(servicesListViewAdapter);
-                            }
+                        }
                     }
                 });
+
+
     }
 
     public void allServicesBtnClicked(View buttonSeeAdminServices) {
@@ -104,6 +150,30 @@ public class ServicesActivityNonAdmin extends AppCompatActivity {
         startActivity(openServicesWindow);
     }
 
+    public void deleteFromDB(final String category, final String service) {
 
+        db.collection("users")
+                .document(getIntent().getStringExtra("CurrentUser_UID")).get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
 
+                        Map empData = documentSnapshot.getData();
+                        Map services = (Map) documentSnapshot.get("Services");
+                        Map servicesWithinCategoryMap = (Map) services.remove(category);
+
+                        empData.remove("Services");
+
+                        if (service == null){
+                            services.remove(category);
+
+                        }else {
+                            servicesWithinCategoryMap.remove(service);
+                            services.put(category,servicesWithinCategoryMap);
+                        }
+                        empData.put("Services", services);
+                        db.collection("users").document(getIntent().getStringExtra("CurrentUser_UID")).set(empData);
+                    }
+                });
+    }
 }
