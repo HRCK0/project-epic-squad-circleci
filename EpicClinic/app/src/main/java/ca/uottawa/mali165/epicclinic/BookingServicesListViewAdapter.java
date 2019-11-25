69db98d2e5259b0d230907a192c4366a21196691
gabrielgranata <ggran067@uottawa.ca>
2019-11-25 17:56:26 -0500

    list view adapter for booking services

--- EpicClinic/app/src/main/java/ca/uottawa/mali165/epicclinic/BookingServicesListViewAdapter.java
+++ EpicClinic/app/src/main/java/ca/uottawa/mali165/epicclinic/BookingServicesListViewAdapter.java
@@ -1,7 +1,11 @@
 package ca.uottawa.mali165.epicclinic;
 
 import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -15,6 +19,24 @@ public class BookingServicesListViewAdapter extends ArrayAdapter {
         super(context, R.layout.service_template, serviceList);
         this.context = context;
         this.serviceList = serviceList;
+
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+
+        LayoutInflater inflater = context.getLayoutInflater();
+        View rowView = inflater.inflate(R.layout.service_template_booking, null, true);
+
+        TextView serviceNameTextField = (TextView) rowView.findViewById(R.id.serviceNameBooking);
+        TextView priceTextField = (TextView) rowView.findViewById(R.id.priceBooking);
+        TextView categoryTextField = (TextView) rowView.findViewById(R.id.categoryBooking);
+
+        serviceNameTextField.setText(serviceList.get(position).getName());
+        priceTextField.setText(serviceList.get(position).getPrice());
+        categoryTextField.setText(serviceList.get(position).getCategory());
+
+        return rowView;
 
     }
 }
--- EpicClinic/app/src/main/java/ca/uottawa/mali165/epicclinic/PatientServicesActivity.java
+++ EpicClinic/app/src/main/java/ca/uottawa/mali165/epicclinic/PatientServicesActivity.java
@@ -1,16 +1,23 @@
 package ca.uottawa.mali165.epicclinic;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class PatientServicesActivity extends AppCompatActivity {
 
@@ -22,13 +29,61 @@ public class PatientServicesActivity extends AppCompatActivity {
 
     ListView listView;
 
-    FirebaseAuth mAuth;
+    FirebaseAuth auth;
     FirebaseFirestore db;
-    Admin user;
+    Patient user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_services);
+
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        listView = findViewById(R.id.listViewBookingServices);
+
+        updateUI();
+
+    }
+
+    public void updateUI() {
+
+        final Activity t = this;
+
+        db.collection("services")
+                .document("services").get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            ArrayList<Service> list = new ArrayList<>();
+
+                            Map categories = task.getResult().getData();
+
+                            for (Object category : categories.keySet()) {
+
+                                Map categoryData = (Map) categories.get(category);
+
+                                for (Object service : categoryData.keySet()) {
+                                    Map serviceData = (Map) categoryData.get(service);
+
+                                    String serviceName = (String) serviceData.get("name");
+                                    String price = (String) serviceData.get("price");
+                                    list.add(new Service(serviceName, price, category.toString()));
+
+
+                                }
+                            }
+                            serviceList = list;
+                        }
+                        BookingServicesListViewAdapter adapter = new BookingServicesListViewAdapter(t, serviceList);
+                        listView.setAdapter(adapter);
+                    }
+
+                });
+
+
     }
 }
